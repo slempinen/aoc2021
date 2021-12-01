63b747588db0567ddb8fe95c29537cc2b771d871
slempinen <santtutek@fastmail.com>
2021-12-01 20:01:24 +0200

    Generalize day 1 solution

--- src/Day1.hs
+++ src/Day1.hs
@@ -4,13 +4,14 @@ module Day1
     ) where
 
 countIncreases :: [Int] -> Int
-countIncreases (a:b:xs) = if a < b then 1 + countIncreases ( b:xs ) else countIncreases ( b:xs )
-countIncreases xs = 0
+countIncreases = f 1
 
 countSlidingIncreases :: [Int] -> Int
-countSlidingIncreases (a1:a2:a3:a4:xs) = if  sum1 < sum2
-    then 1 + countSlidingIncreases ( a2:a3:a4:xs )
-    else countSlidingIncreases ( a2:a3:a4:xs )
-    where sum1 = a1+a2+a3
-          sum2 = a2+a3+a4
-countSlidingIncreases xs = 0
+countSlidingIncreases = f 3
+
+f :: Int -> [Int] -> Int 
+f winSize xs
+    | length xs < winSize = 0
+    | otherwise = if sum1 < sum2 then 1 + f winSize (tail xs) else f winSize (tail xs)
+        where sum1 = sum $ take winSize xs
+              sum2 = sum $ take winSize $ drop 1 xs
